013275c1d1c721f28d6c45f68fb79e632ec8c3eb
Deason Holt <holde342@student.otago.ac.nz>
2026-09-19 13:08:54 +1200

    Completed basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,28 +1,43 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public bool FreeMovement = false;
+    
+    public PlayerControls Inputs;
     
     public float moveSpeed = 5f;
 
     public int PlayerVal;
     
     private Vector2 _moveDirection;
-    
-    
-    public void HandleMovement(InputAction.CallbackContext context)
-    {
-        _moveDirection = context.readValue<Vector2>();
-    }
-
+    private Rigidbody2D rigidbody;
 
     public void Update()
     {
         if (_moveDirection.sqrMagnitude > 0f)
         {
             float angle = Mathf.Atan2(_moveDirection.y, _moveDirection.x) * Mathf.Rad2Deg - 90f;
+            
         }
+    }
+
+
+    private void Awake()
+    {
+        Inputs = new PlayerControls();
+        Inputs.GamePlay.Move.performed += HandleMove;
+        Inputs.Enable();
+        Debug.Log("PlayerController::Awake");
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void HandleMove(InputAction.CallbackContext context)
+    {
+        _moveDirection = context.ReadValue<Vector2>();
+        rigidbody.AddForce(_moveDirection.normalized * moveSpeed, ForceMode2D.Impulse);
+        Debug.Log(_moveDirection);
     }
 }
