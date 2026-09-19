aeec5d362344a712090d22f5b2c75d1fb7adddf6
Gavin <mastergavincao@gmail.com>
2026-09-19 14:56:37 +1200

    Fishworld Sprites are done

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelChange : Monobehaviour
+public class LevelChange : MonoBehaviour
 {
-    
-    private void OnTriggerEnter2D(Collider2D col){
-           if (col.gameObject.tag == "Player")
-            SceneManager.LoadScene();
+    private void OnTriggerCollider2D(Collider2D col){
+        if (col.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("");
+        }
     }
 }
