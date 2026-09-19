74eb98f68363ee27f70e67bd2e4c3b75e6539a5f
Gavin <mastergavincao@gmail.com>
2026-09-20 10:51:29 +1200

    Added in a start to the main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEditor.Playables;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public Button StartButton;
+    public Button EndButton;
+
+    private void Start()
+    {
+        StartButton.onClick.AddListener(LoadGame);
+        EndButton.onClick.AddListener(QuitGame);
+    }
+    public void LoadGame()
+    {
+        SceneManager.LoadSceneAsync("Cambrian");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
