4086262d5adab935a4ec94ab3e17ec45fe06255e
Gavin <mastergavincao@gmail.com>
2026-09-19 21:36:54 +1200

    Can now swap scenes so levels can now function.

--- Assets/Scripts/LevelChange.cs
+++ Assets/Scripts/LevelChange.cs
@@ -3,10 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class LevelChange : MonoBehaviour
 {
-    private void OnTriggerCollider2D(Collider2D col){
-        if (col.gameObject.CompareTag("Player"))
+    public string LevelName;
+    void Update()
+    {
+        if (Time.frameCount % 10 == 0)
         {
-            SceneManager.LoadScene("");
+            SpinDNA();
         }
+    }
+
+    void SpinDNA()
+    {
+        bool left = false;
+
+        if(left == false)
+        {
+            gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            left = true;
+        }
+        else
+        {
+            gameObject.transform.localScale = new Vector3(1, 1, 1);
+            left = false;
+        }
+        
+    }
+
+    [System.Obsolete]
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log($"DNA Triggered");
+        SceneManager.LoadScene("Jurassic");
+        SceneManager.UnloadScene("Cambrian");
     }
 }
